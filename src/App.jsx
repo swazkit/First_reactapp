b0bada09ba627a8d52a130153e705943325cd4f1
swazkit <swastikraychoudhuri@gmail.com>
2022-11-08 01:35:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Components/Header';
+import Main from './Components/Main';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+      <header>
+              <nav>
+                  <div className="react">
+                    <i className="fa-brands fa-react logo"></i>
+                    <h1>ReactFacts</h1>
+                  </div>
+                  <p>React Course - Project 1</p>
+              </nav>
+      </header>
+    )
+  }
+  
